824e8831ce97a431a44958939753203fb82d3865
kodlanil <104692307+kodlanil@users.noreply.github.com>
2022-05-04 22:11:17 +0300

    Update flatten.py

--- flatten.py
+++ flatten.py
@@ -1,11 +1,18 @@
-result=[]
 def flatten(lst):
-  for element in lst:
-    if type(element) in (list,set, tuple):
-      flatten(element)
-    elif type(element)==dict:
-      for x in d.items():
-        flatten(x)
-    else:
-      result.append(element)
-  return result
+  while True:
+    r=0
+    new_lst=[]
+    for element in lst:
+      if type(element) in (list,set, tuple):
+        r+=1
+        new_lst.extend(element)
+      elif type(element)==dict:
+        for x in element.items():
+          new_lst.extend(x)
+        r+=1
+      else:
+        new_lst.append(element)
+    lst=new_lst
+    if r==0:
+      break
+  return lst
